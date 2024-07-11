4ce037a733a15002248a2d31163aec720aea552a
Lakiza Vladislav <brampaap45@gmail.com>
2024-07-11 13:50:15 +0300

    .

--- index.js
+++ index.js
@@ -31,12 +31,14 @@ app.get("/quest", (req, res) => {
 });
 
 app.get("/status", (req, res) => {
+    res.set('Content-Type', 'text/html');
     return res.status(200).send(SERVER_DATA);
 });
 
 app.get("/restart", (req, res) => {
     SERVER_DATA.current_quest_index = 0;
     SERVER_DATA.statuses = {};
+    res.set('Content-Type', 'text/html');
     return res.status(200).send(JSON.stringify(SERVER_DATA));
 });
 
